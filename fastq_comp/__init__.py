85f86530b99ec7b2f4524e61b317f99f089462a8
Abhiram Das <adas@dnanexus.com>
2016-01-07 11:00:46 -0800

    Updated the test method

--- fastq_comp/__init__.py
+++ fastq_comp/__init__.py
@@ -45,6 +45,6 @@ def fqz_compress(in_file, out_file):
 def test():
     """It is a test function to just run a shell script
     """
-    cmd = BASE_EXE_LOC + "test.sh"
-    cmd_out = subprocess.check_output([cmd])
+    cmd = BASE_EXE_LOC + FQZ_EXE
+    cmd_out = subprocess.check_output([cmd, "-h"])
     print(cmd_out)
